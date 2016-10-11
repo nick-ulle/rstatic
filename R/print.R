05369fab80007f328dd6911cde82a3b55cf98d72
Nick Ulle <nick.ulle@gmail.com>
2016-10-11 16:51:46 -0700

    Correct signature for print method.

--- R/print.R
+++ R/print.R
@@ -3,9 +3,9 @@
 #
 
 #' @export
-print.ASTNode = function(object) {
-  fields = paste("$", ls(object), sep = "", collapse = " ")
-  cat(sprintf("<%s> %s\n", class(object)[1], fields))
-  print(to_r(object))
+print.ASTNode = function(x, ...) {
+  fields = paste("$", ls(x), sep = "", collapse = " ")
+  cat(sprintf("<%s> %s\n", class(x)[1], fields))
+  print(to_r(x))
 }
 
