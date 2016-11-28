bb33f9a1dca18b2a9b8c1aacc4e9e2cbff59a6a6
Nick Ulle <nick.ulle@gmail.com>
2016-11-28 14:32:56 -0800

    Add conversion from CFGraph to igraph objects.

--- R/as_igraph.R
+++ R/as_igraph.R
@@ -1,17 +1,29 @@
 
-#as_igraph = function(x) {
-#  if (!requireNamespace("igraph", quietly = TRUE))
-#    stop('This function requires the "igraph" package. Please install it.')
-#
-#  UseMethod("as_igraph")
-#}
-#
-#
-#as_igraph.BasicBlock = function(x) {
-#  ig = igraph::make_empty_graph()
-#
-#  # Traverse the graph to get nodes and edges.
-#  while () {
-#
-#  }
-#}
+
+#' Convert CFGraph to igraph
+#'
+#' This function converts a CFGraph object to an igraph object, which is useful
+#' for plotting the graph. The resulting igraph does not retain the code inside
+#' the basic blocks, only the structure of the original CFGraph.
+#'
+#' @param cfg (CFGraph) a graph to convert
+#'
+#' @export
+as_igraph = function(cfg) {
+  if (!requireNamespace("igraph", quietly = TRUE))
+    stop('This function requires the "igraph" package. Please install it.')
+
+  # Get edge list.
+  edges = lapply(seq_along(cfg$blocks),
+    function(i) {
+      pred = cfg[[i]]$predecessors
+      if (length(pred) == 0)
+        return (NULL)
+
+      cbind(pred, i)
+    }
+  )
+  edges = do.call(rbind, edges)
+
+  return (igraph::graph_from_edgelist(edges, directed = TRUE))
+}
