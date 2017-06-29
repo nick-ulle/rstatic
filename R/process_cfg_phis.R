5f2acae82a91187d2536a344aa4144728fc13b3b
Duncan Temple Lang <duncan@r-project.org>
2017-06-28 19:34:06 -0700

    code to determine the names of the variables that are used in Phi nodes

---
+++ R/process_cfg_phis.R
@@ -0,0 +1,22 @@
+findPhiAssignVarNames =
+function(cfg, ssa.graph = cfg$ssa$graph)
+{
+  phis = lapply(cfg$blocks, function(x) x$phi)
+  w = sapply(phis, length) > 0
+
+    # Find the p
+  lapply(phis[w], function(x) lapply(x, function(x) getPhiAssign(x$write$name, cfg, ssa.graph)))
+  
+}
+
+  
+
+# If we had a reference to the Symbol in the read, we could mark that directly.
+
+
+getPhiAssign =
+function(sym, cfg, ssa.graph = cfg$ssa$graph)
+{
+  defs = igraph::neighbors(ssa.graph, sym, "in")
+  as_ids(defs)
+}
