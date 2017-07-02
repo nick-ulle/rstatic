5fcf93043587738bdd343f5819e4bd3170244d71
Duncan Temple Lang <duncan@r-project.org>
2017-07-01 20:38:44 -0700

    new astTraverse methods.  update to replaceNode to now use field to allow setting different elements of the ASTNode. Self-determines by default

--- R/ASTTraverse.R
+++ R/ASTTraverse.R
@@ -62,30 +62,81 @@ function(ast, fun, ...)
   warning("no method for astTraverse for ",  class(ast)[1])
 }
 
-insertBefore =
-function(into, beforeNode, value)
+astTraverse.While =
+function(ast, fun, ...)
+{
+    astTraverse(ast$condition, fun, ...)
+    astTraverse(ast$body, fun, ...)    
+}
+
+
+astTraverse.If =
+function(ast, fun, ...)
+{
+  astTraverse(ast$condition, fun, ...)
+  astTraverse(ast$true, fun, ...)
+  if(length(ast$false))
+      astTraverse(ast$false, fun, ...)  
+}
+
+########################
+
+insertNode =
+function(into, atNode, value, before = TRUE, field = "body")
 {
 
+  w = sapply(into$body, identical, atNode)
+
+  if(!any(w))
+     stop("atNode is not in the object")
+  if(sum(w) > 1)
+     stop("more than one matching node")
+
+  into$body = if(before) {
+                 tmp = split(into$body, cumsum(w))      
+                 c(tmp[[1]], value, tmp[[2]])
+              } else {
+                 vals = into$body
+                 if(w[1])
+                     c(vals[[1]], value, vals[-1])
+                 else {
+                     i = seq(1, length = which(w))
+                     c(vals[i], value, vals[-i])
+                 }
+              }
 }
+
+
 replaceNode =
     # Eventually we will use field to allow setting arbitrary fields
     # We'll use get(field, into) and look for set_ and get_ methods for that field.
-function(into, node, value, multi = is.list(value), field = "body")
+function(into, node, value, multi = is.list(value),
+         field = switch(class(into)[1],
+                        Call = "args",
+                        "body"),
+         error = TRUE)
 {
-  w = sapply(into$body, identical, node) #XXX more general than body.
-  if(!any(w))
-      stop("node is not in the object")
+  vals = get(field, into)
+  w = sapply(vals, identical, node) #XXX more general than body.
+  if(!any(w)) {
+      if(error)
+          stop("node is not in the object")
+      else  # XXX there are recursive situations where we change the node and then cannot find it in subsequent calls. See makeIntegerLiterals in R2llvm/R/rewrite.R
+          return(NULL)
+  }
   if(sum(w) != 1)
       stop("more than one matching node")
 
   if(multi) {
-      tmp = split(into$body, cumsum(w))
-      into$body = c(tmp[[1]],  value, tmp[[2]][-1])
+      tmp = split(vals, cumsum(w))
+      nvals = c(tmp[[1]],  value, tmp[[2]][-1])
       sapply(value, function(x) x$parent = into)
   } else {
-    into$body[[which(w)]] = value
+    vals[[which(w)]] = value
+    nvals = vals
     value$parent = into
   }
+  assign(field, nvals, into)
 
   NULL
 }
