af21c7de461966bd8802d504a57e69da94584b37
Nick Ulle <nick.ulle@gmail.com>
2017-11-01 16:27:24 -0700

    Default to id '%entry' for CFG entry block.

--- R/flow_graph.R
+++ R/flow_graph.R
@@ -22,8 +22,8 @@ FlowGraph = R6::R6Class("FlowGraph",
 
     copy = function() self$clone(deep = TRUE),
 
-    add_vertex = function(id) {
-      if (missing(id)) {
+    add_vertex = function(id = NULL) {
+      if (is.null(id)) {
         id = sprintf("%%%i", self$next_id)
         self$next_id = self$next_id + 1L
       }
@@ -35,13 +35,13 @@ FlowGraph = R6::R6Class("FlowGraph",
 
     remove_vertex = function(id) {
       self$graph = self$graph - igraph::vertex(id)
-      invisible (NULL)
+      invisible(NULL)
     },
 
     add_edge = function(from, to) {
       self$graph = self$graph + igraph::edge(from, to)
 
-      invisible (NULL)
+      invisible(NULL)
     },
 
     get_index = function(name) {
@@ -60,7 +60,7 @@ FlowGraph = R6::R6Class("FlowGraph",
       permutation[ordering] = permutation
       self$graph = permute.vertices(self$graph, permutation)
 
-      NULL
+      invisible(NULL)
     }
   )
 )
@@ -131,16 +131,17 @@ ControlFlowGraph = R6::R6Class("ControlFlowGraph", inherit = FlowGraph,
     },
 
     add_block = function(block = Brace$new(), id = NULL) {
-      # FIXME: Use defaults, not missing.
-      if (missing(id))
-        id = self$add_vertex()
-      else
-        self$add_vertex(id)
+      if (is.null(self$entry)) {
+        if (is.null(id)) {
+          id = "%entry"
+          self$entry = id
+        } else {
+          self$entry = id
+        }
+      }
 
+      id = self$add_vertex(id)
       self$blocks[[id]] = block
-
-      if (is.null(self$entry))
-        self$entry = id
 
       id
     }
