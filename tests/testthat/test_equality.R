42991fea4d155b74b15e858e3b0909afb2fbf384
Nick Ulle <nick.ulle@gmail.com>
2018-02-19 17:19:22 -0800

    Add some equality methods.

---
+++ tests/testthat/test_equality.R
@@ -0,0 +1,75 @@
+context("equality")
+
+
+test_that("Literals", {
+  # TRUE -----
+  result = Integer$new(1) == Integer$new(1)
+  expect_true(result)
+
+  result = Character$new("hi") == Character$new("hi")
+  expect_true(result)
+
+
+  # FALSE -----
+  result = Character$new("bye") == Character$new("hi")
+  expect_false(result)
+
+  result = Character$new("yby") == Numeric$new(3)
+  expect_false(result)
+})
+
+
+test_that("Symbols", {
+  # TRUE -----
+  result = Symbol$new("x") == Symbol$new("x")
+  expect_true(result)
+
+  # FALSE -----
+  result = Symbol$new("x", 1) == Symbol$new("x", 2)
+  expect_false(result)
+
+  result = Parameter$new("x", default = Integer$new(1)) ==
+    Parameter$new("x", default = Integer$new(2))
+  expect_false(result)
+
+  # TODO: Namespaces
+})
+
+
+test_that("Assignments", {
+  # TRUE -----
+  result = Assign$new("y", "hello") == Assign$new("y", "hello")
+  expect_true(result)
+
+  # FALSE -----
+  result = Assign$new("x", 3) == Assign$new("x", 4)
+  expect_false(result)
+
+  result = Assign$new("z", 3) == Assign$new("x", 3)
+  expect_false(result)
+})
+
+
+test_that("Calls", {
+  # TRUE -----
+  c1 = Call$new("f")
+  c2 = Call$new("f")
+  result = c1 == c2
+  expect_true(result)
+
+  c1 = Call$new("f", list(Symbol$new("x")))
+  c2 = Call$new("f", list(Symbol$new("x")))
+  result = c1 == c2
+  expect_true(result)
+
+  # FALSE -----
+  c1 = Call$new("f", list(Symbol$new("x")))
+  c2 = Call$new("f", list(Integer$new(3)))
+  result = c1 == c2
+  expect_false(result)
+
+  c1 = Call$new("g", list(Symbol$new("x")))
+  c2 = Call$new("f", list(Symbol$new("x")))
+  result = c1 == c2
+  expect_false(result)
+})
