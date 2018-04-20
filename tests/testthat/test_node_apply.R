f3e9e51dd095790e8170cda6931dcd3c04683098
Clark Fitzgerald <clarkfitzg@gmail.com>
2018-04-20 09:59:57 -0700

    actual missing argument

--- tests/testthat/test_node_apply.R
+++ tests/testthat/test_node_apply.R
@@ -4,9 +4,11 @@ context("node_apply")
 test_that("Missing arguments", {
 
     node = quote_ast(x[1, "column"])
-
     node$args[[2]]
+    node_apply(node, print)
 
+    node = quote_ast(x[, "column"])
+    node$args[[2]]
     node_apply(node, print)
 
 })
