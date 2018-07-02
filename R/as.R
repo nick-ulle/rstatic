a3c6b4e8e99fe1ff97caa333c83fd03008eb2a93
Nick Ulle <nick.ulle@gmail.com>
2018-07-01 19:39:26 -0700

    Add S3 as-methods.

---
+++ R/as.R
@@ -0,0 +1,138 @@
+#' @export
+as.call =
+function(x)
+{
+  UseMethod("as.call")
+}
+
+#' @export
+as.call.Call =
+function(x)
+{
+  as_language.Call(x)
+}
+
+#' @export
+as.call.default = base::as.call
+
+
+# as names/symbols ----------------------------------------
+
+#' @export
+as.name =
+function(x)
+{
+  UseMethod("as.name")
+}
+
+#' @export
+as.name.Symbol =
+function(x)
+{
+  base::as.name(x$basename)
+}
+
+#' @export
+as.name.default = base::as.name
+
+#' @export
+as.symbol =
+function(x)
+{
+  UseMethod("as.symbol")
+}
+
+#' @export
+as.symbol.Symbol =
+function(x)
+{
+  base::as.symbol(x$basename)
+}
+
+#' @export
+as.symbol.default = base::as.symbol
+
+
+# as other literals ----------------------------------------
+
+#' @export
+as.character.Symbol =
+function(x, ...)
+{
+  x$basename
+}
+
+#' @export
+as.character.Character =
+function(x, ...)
+{
+  x$value
+}
+
+#' @export
+as.character.Literal =
+function(x, ...)
+{
+  as.character(x$value, ...)
+}
+
+
+#' @export
+as.complex.Complex =
+function(x, ...)
+{
+  x$value
+}
+
+
+#' @export
+as.complex.Literal =
+function(x, ...)
+{
+  as.complex(x$value)
+}
+
+
+#' @export
+as.double.Numeric =
+function(x, ...)
+{
+  x$value
+}
+
+#' @export
+as.double.Literal =
+function(x, ...)
+{
+  as.double(x$value, ...)
+}
+
+
+#' @export
+as.integer.Integer =
+function(x, ...)
+{
+  x$value
+}
+
+#' @export
+as.integer.Literal =
+function(x, ...)
+{
+  as.integer(x$value, ...)
+}
+
+
+#' @export
+as.logical.Logical =
+function(x, ...)
+{
+  x$value
+}
+
+#' @export
+as.logical.Literal =
+function(x, ...)
+{
+  as.logical(x$value, ...)
+}
