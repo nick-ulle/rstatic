492a8f78555c07b11eca2bfcbcdb34332ccaeb3c
Nick Ulle <nick.ulle@gmail.com>
2019-10-23 18:27:35 -0700

    WIP Decouple block aggregation from dfa_gen_kill.

--- R/flow_analysis.R
+++ R/flow_analysis.R
@@ -7,41 +7,47 @@
 #'
 #' @param cfg (BlockList) A control flow graph with which to compute a
 #' solution.
-#' @param initial Initial guess of solution for each block.
-#' @param gen (logical matrix) The gen sets as a logical matrix. Rows
-#' correspond to items and columns correspond to blocks.
-#' @param kill (logical matrix) The kill sets as a logical matrix. Rows
-#' correspond to items and columns correspond to blocks.
+#' @param initial (logical matrix) Initial guess of solution for each block.
+#' @param gk_list (list of GenKillSets) A list of the gen and kill sets for
+#' each block.
 #' @param forward (logical) Is this a forward data flow analysis? If `FALSE`,
 #' the solver will traverse the control flow graph backward.
 #' @param confluence (function) A function to combine sets at a merge in
 #' control flow. Defaults to `|`, which unions the sets.
-#' @param ... Additional arguments to `update`.
 #' @param update (function) A function to update a block's set based on the gen
 #' and kill sets. Defaults to `dfa_standard_update`.
+#' @param ... Additional arguments to `update`.
+#' @param aggregate (logical) Aggregate results to the block-level?
 #' @param max_iter (integer) Maximum number of iterations to run.
-#' @param full_analysis (logical) If `FALSE`, either the entry sets or exit
-#' sets are computed, but not both. Entry sets are always computed for a
-#' forward analysis, and exit sets are always computed for a backward analysis.
 #'
-#' @return A two-element list. The first element, "entry", is a logical matrix
-#' that represents the solution sets at the entry of each block. The second
-#' element, "exit", is a logical matrix that represents the solution sets at
-#' the exit of each block. For both matrices, rows correspond to set items and
-#' columns correspond to blocks.
+#' @return If `aggregate = FALSE` (the default), a list of logical matrices.
+#' Each list element corresponds to one block in `cfg`. For each matrix, rows
+#' correspond to set items. Columns represent entry sets for lines in the
+#' block; the last column is the exit set for the last line (equivalently, for
+#' the block).
 #'
-#' @seealso [dfa_gen_kill()], [dfa_standard_update()]
+#' If `aggregate = TRUE`, a two-element list. The first element, "entry", is a
+#' logical matrix that represents the solution sets at the entry of each block.
+#' The second element, "exit", is a logical matrix that represents the solution
+#' sets at the exit of each block. For both matrices, rows correspond to set
+#' items and columns correspond to blocks.
+#'
+#' @seealso [dfa_initialize()], [dfa_gen_kill()], [dfa_standard_update()]
 #' @export
 dfa_solve =
-function(cfg, initial, gen, kill
+function(cfg, initial, gk_list
   , forward = TRUE
   , confluence = `|`
-  , ...
   , update = dfa_standard_update
-  , full_analysis = TRUE, max_iter = 1000L)
+  , ...
+  , aggregate = FALSE, max_iter = 1000L)
 {
   if (is(cfg, "BlockList"))
     cfg = compute_cfg(cfg)
+
+  gk = dfa_aggregate(gk_list, forward = forward)
+  gen = gk@gen
+  kill = gk@kill
 
   if (forward) {
     # For forward analysis, source vertex comes first.
@@ -55,6 +61,7 @@ function(cfg, initial, gen, kill
     edge_mode = "in"
   }
 
+  # <<<--- MFP solver -------------------------
   # Step 1: Initialize worklist of edges.
   worklist = igraph::ends(cfg, igraph::E(cfg))
 
@@ -82,16 +89,19 @@ function(cfg, initial, gen, kill
 
     iter = iter + 1L
   }
+  # ---------------------------------------->>>
 
-  # Step 3: The exit sets can be computed by an update() on each entry set.
-  exit = if (full_analysis) {
-    update(initial, gen, kill)
-  } # else NULL
+  if (aggregate) {
+    # The exit sets can be computed by an update() on each entry set.
+    updated = update(initial, gen, kill, ...)
 
-  if (forward)
-    list(entry = initial, exit = exit)
-  else
-    list(entry = exit, exit = initial)
+    if (forward)
+      return (list(entry = initial, exit = updated))
+    else
+      return (list(entry = updated, exit = initial))
+  }
+
+  dfa_disaggregate(initial, gk_list, forward = forward, update = update, ...)
 }
 
 
@@ -154,39 +164,121 @@ function(result, gen, kill, ...)
 }
 
 
-#' Aggregate Data Flow Analysis Sets to Block-level
+#' Aggregate Data Flow Analysis Gen and Kill Sets to Block-level
 #'
-#' This function takes line-level sets from a data flow analysis and aggregates
-#' them to block-level by sequential application of the update function.
+#' This function aggregates line-level gen and kill sets for a data flow
+#' analysis to block-level by propagating them through the block.
 #'
+#' @param gen (logical matrix) Gen sets for each line.
+#' @param kill (logical matrix) Kill sets for each line.
+#' @param forward (logical) Should sets be aggregated forward (first to last
+#' line) or backward (last to first line)?
+#' @param ... Additional arguments to [dfa_propagate()].
+#'
+#' @return A logical vector with `nrow(gen)` elements. The set computed by
+#' aggregating over all of the line-level sets.
+#'
+#' @seealso [dfa_propagate()], [dfa_standard_update()]
+#' @export
+dfa_aggregate =
+function(gk_list, forward = TRUE, ...)
+{
+  gen1 = gk_list[[1L]]@gen
+  n_items = nrow(gen1)
+  n_blocks = length(gk)
+
+  gen = matrix(NA, n_items, n_blocks)
+  kill = matrix(NA, n_items, n_blocks)
+  rownames(gen) = rownames(gen1)
+  rownames(kill) = rownames(gen1)
+
+  # Aggregate each block.
+  for (b in seq_len(n_blocks)) {
+    gk = gk_list[[b]]
+
+    sets = dfa_propagate(initial = FALSE, gk = gk, forward = forward, ...)
+    if (forward)
+      gen[, b] = sets[, ncol(sets)]
+    else
+      gen[, b] = sets[, 1L]
+
+    kill[, b] = row_ors(gk@kill)
+  }
+
+  GenKillSets(gen, kill)
+}
+
+
+#' @export
+dfa_disaggregate =
+function(sets, gk_list, ...)
+# Propagate results within each block to get back to line-level information.
+{
+  n_blocks = ncol(sets)
+  result = vector("list", n_blocks)
+  for (j in seq_len(n_blocks))
+    result[[j]] = dfa_propagate(sets[, j], gk_list[[j]], ...)
+
+  result
+}
+
+#' Propagate Data Flow Analysis Result Sets within a Block
+#'
+#' This function propagates result sets from a data flow analysis at line-level
+#' by sequential application of the update function.
+#'
+#' @param initial (logical matrix) Initial set to propagate. For forward
+#' propagation, this is the entry set for the block. For backward propagation,
+#' this is the exit set for the block.
 #' @param gen (logical matrix) Gen sets for each line.
 #' @param kill (logical matrix) Kill sets for each line.
 #' @param forward (logical) Should sets be aggregated forward (first to last
 #' line) or backward (last to first line)?
 #' @param update (function) A function to compute the update set for a block.
 #'
-#' @return A logical vector with `nrow(gen)` elements. The set computed by
-#' aggregating over all of the line-level sets.
+#' @return A logical matrix with `nrow(gen)` rows and `ncol(gen) + 1` columns.
+#' Rows correspond to set items. Columns represent entry sets for lines; the
+#' last column is the exit set for the last line (equivalently, for the block).
 #'
-#' @seealso [dfa_standard_update()]
+#' @seealso [dfa_aggregate()], [dfa_disaggregate()], [dfa_standard_update()]
 #' @export
-dfa_aggregate =
-function(gen, kill, forward = TRUE, update = dfa_standard_update)
+dfa_propagate =
+function(initial
+  , gk
+  , forward = TRUE
+  , update = dfa_standard_update
+  , ...)
 {
+  gen = gk@gen
+  kill = gk@kill
+
+  # Add column for initial sets.
   n = ncol(gen)
+  sets = matrix(NA, nrow(gen), n + 1L)
+  rownames(sets) = rownames(gen)
+
   if (forward) {
+    sets[, 1L] = initial
     index = seq(1L, n)
-    initial = gen[, 1L]
+
+    for (j in index)
+      # Here:
+      #   sets[, j + 1L] is exit set for line j
+      #    gen[, j     ] is gen set for line j
+      sets[, j + 1L] = update(sets[, j], gen[, j], kill[, j], ...)
+
   } else {
-    index = seq(n, 1L)
-    initial = gen[, n]
+    sets[, n + 1L] = initial
+    index = seq(n + 1L, 2L)
+
+    for (j in index)
+      # Here:
+      #   sets[, j - 1L] is entry set for line j - 1L
+      #    gen[, j - 1L] is gen set for line j - 1L
+      sets[, j - 1L] = update(sets[, j], gen[, j - 1L], kill[, j - 1L], ...)
   }
 
-  for (j in index) {
-    initial = update(initial, gen[, j], kill[, j])
-  }
-
-  initial
+  sets
 }
 
 
@@ -212,37 +304,55 @@ function(gen, kill, forward = TRUE, update = dfa_standard_update)
 #' @seealso [dfa_solve()]
 #' @export
 dfa_gen_kill =
-function(cfg, fun, universe, forward = TRUE, ...)
+function(x, fun, universe, ...)
+{
+  UseMethod("dfa_gen_kill")
+}
+
+
+# FIXME: Document methods separately?
+#' @export
+dfa_gen_kill.BlockList =
+function(x, fun, universe, ...)
+{
+  lapply(x$contents, dfa_gen_kill, fun, universe, ...)
+}
+
+
+#' @export
+dfa_gen_kill.Block =
+function(x, fun, universe, ...)
+  # Compute line-level gen and kill sets for a single block.
 {
   n_items = length(universe)
 
-  n_blocks = length(cfg$contents)
-  gen = matrix(NA, n_items, n_blocks)
-  kill = matrix(NA, n_items, n_blocks)
+  # Compute gen and kill sets for each line.
+  n_lines = length(x)
+  gen = matrix(NA, n_items, n_lines)
+  kill = matrix(NA, n_items, n_lines)
   rownames(gen) = names(universe)
   rownames(kill) = names(universe)
 
-  # Compute kill and gen sets for each block.
-  for (b in seq_len(n_blocks)) {
-    block = cfg[[b]]
+  for (j in seq_len(n_lines)) {
+    line = x[[j]]
+    gk = fun(line, universe, ...)
 
-    # Compute kill and gen sets for each line.
-    n_lines = length(block)
-    b_kill = matrix(NA, n_items, n_lines)
-    b_gen = matrix(NA, n_items, n_lines)
-
-    for (j in seq_len(n_lines)) {
-      line = block[[j]]
-      kg = fun(line, universe, ...)
-
-      b_gen[, j] = kg[["gen"]]
-      b_kill[, j] = kg[["kill"]]
-    }
-
-    # Aggregate line-level sets to block-level.
-    gen[, b] = dfa_aggregate(b_gen, b_kill, forward = forward)
-    kill[, b] = row_ors(b_kill)
+    gen[, j] = gk[["gen"]]
+    kill[, j] = gk[["kill"]]
   }
 
-  list(gen = gen, kill = kill)
+  GenKillSets(gen, kill)
+}
+
+
+setClass("rstatic::GenKillSets",
+  slots = list(
+    gen = "matrix",
+    kill = "matrix"
+  ))
+
+GenKillSets =
+function(gen, kill)
+{
+  new("rstatic::GenKillSets", gen = gen, kill = kill)
 }
