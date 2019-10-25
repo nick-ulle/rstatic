256a1de8c247b6f8d85aca477ae150b94d572186
Nick Ulle <nick.ulle@gmail.com>
2019-10-25 16:03:30 -0700

    Add transform_lines function.

--- R/transformations.R
+++ R/transformations.R
@@ -38,3 +38,70 @@ function(node, replacement, in_place = TRUE)
   parent[[m]] = replacement
   parent
 }
+
+
+
+#' Transform Elements in a `Container` Object
+#'
+#' This function transforms each element in a `Container` object by applying
+#' the given `transform` function.
+#'
+#' This is an in-place transformation!
+#'
+#' This function is intended for transforming lines in `Block` objects, but
+#' also works with other `Container` objects.
+#'
+#' This function can transform a single element in a container into multiple
+#' elements. If the `transform` function returns a list, the original element
+#' is replaced with one element for each element in the list.
+#'
+#' If the `transform` function returns an ASTNode, the original element is
+#' replaced with that node.
+#'
+#' The index of the original element is passed to the `transform` function as
+#' `index`. If the top-level `node` argument was a `BlockList` object, the
+#' block index is also passed to the `transform` function as `block_index`.
+#'
+#'
+#' @param node (ASTNode) The node to transform.
+#' @param transform (function) The transformation function to apply to each
+#' line. See the details section.
+#'
+#' @return The `node` argument after transformation.
+#'
+#' @seealso [replace_nodes()]
+#' @export
+transform_lines =
+function(node, transform, ...)
+{
+  UseMethod("transform_lines")
+}
+
+
+#' @export
+transform_lines.BlockList =
+function(node, transform, ...)
+{
+  for (b in seq_along(node))
+    node[[b]] = transform_lines(node[[b]], transform, ..., block_index = b)
+
+  node
+}
+
+
+#' @export
+transform_lines.Container =
+function(node, transform, ...)
+{
+  indices = rev(seq_along(node))
+
+  for (i in indices) {
+    new_elts = transform(node[[i]]
+      , ...
+      , index = i)
+
+    node$contents = append(node$contents[-i], new_elts, i)
+  }
+
+  node
+}
